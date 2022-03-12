df8edceab300765a17aaa7c14ee82084ed465636
justin__ <31811490+coffeeandc0d3@users.noreply.github.com>
2022-03-11 19:03:44 -0500

    Delete src directory

--- src/tools.py
+++
@@ -1,6 +0,0 @@
-import pygame 
-
-def transform_image(img, scalar):
-	# get size as a tuple of newly scaled image: 
-	size = round(img.get_width() * scalar), round(img.get_height() * scalar)
-	return pygame.transform.scale(img, size)
